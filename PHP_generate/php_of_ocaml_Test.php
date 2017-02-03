ab8233e747a194a6681d7998a9cc3caf7ecf141f
sofiane <ghersa_sofiane@ubuntu.ubuntu-domain>
2017-02-03 22:32:53 +0100

    Ajouter le dossier contien les fichier php

---
+++ PHP_generate/php_of_ocaml_Test.php
@@ -0,0 +1,21 @@
+<?php
+
+$tup_1_elem = 88;
+$tup_2_elem = array ( 'a' , "ocaml" ) ;
+$tup_3_elem = array ( 'a' , "ocaml" , 20 ) ;
+$tup_4_elem = array ( 88 , 5.20 , 'a' , "ocaml" ) ;
+$tab_vide = array (  );
+$tab_int = array ( 4 , 6 , 87);
+$tab_float = array ( 4.4 , 6.5 , 8.7 , 0.5);
+$tab_char = array ( 'a' , 'b' , 'c' , 'd');
+$tab_string = array ( "GHERSA" , "Sofiane" , "KASDI" , "Hacene");
+$notre_int = 5;
+$notre_char = 'C';
+$notre_string = "notre string en OCAML qui traduit en PHP";
+$notre_float = 10.5;
+function add = ( x ){body};
+ eval  
+ eval  
+ type  
+
+?>
