193fd0b7e1da63d2784fa37debbb9753f1b51ddb
sofiane <ghersa_sofiane@ubuntu.ubuntu-domain>
2017-02-03 20:30:23 +0100

    Fichier Test

---
+++ tests/Test.ml
@@ -0,0 +1,28 @@
+let tup_1_elem = ( 88)
+let tup_2_elem = ( 'a', "ocaml")
+let tup_3_elem = ( 'a', "ocaml", 20)
+let tup_4_elem = ( 88, 5.20, 'a', "ocaml")
+
+(************************************************)
+
+let tab_vide = [||]
+let tab_int = [|4;6;87|]
+let tab_float = [|4.4;6.5;8.7;0.5|]
+let tab_char = [|'a';'b';'c';'d'|]
+let tab_string = [|"GHERSA";"Sofiane";"KASDI";"Hacene"|]
+
+
+
+(************************************************)
+
+let notre_int = 5 ;;(* entier *)
+let notre_char = 'C' ;;(* caractère *)
+let notre_string = "notre string en OCAML qui traduit en PHP" ;;(* chaîne de caractère *)
+let notre_float = 10.5 ;;(* réel *)
+
+let add x y z = 5;;
+
+[|2;4;5;9|];;
+5;;
+type client = {numero: int; nom: string; solde: float};;
+
