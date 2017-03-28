0a0e60f5245071eadcaf33a60dfc80814ed5b3e0
sofiane <ghersa_sofiane@ubuntu.ubuntu-domain>
2017-03-28 18:00:33 +0200

    ajout return et conversions et Variable

---
+++ tests/Variable.ml
@@ -0,0 +1,10 @@
+
+
+let x = let y = 5 in 5 + y ;;
+
+let x = let y = 5 in let z = 10 in z + y ;;
+
+let x = let y = 5 in let z = 10 in let p = 20 in p+z + y ;;
+
+let x = let y = 5 in let z = 10 in let p = 20 in let g = 5 in g+ p+z + y ;; 
+
